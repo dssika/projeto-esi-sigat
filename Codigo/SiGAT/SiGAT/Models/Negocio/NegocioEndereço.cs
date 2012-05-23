26dab40f31fc44c08e0ed700fa7b408811207f95
eliasware@hotmail.com <eliasware@hotmail.com@402bc129-2925-e306-34ad-f945a9757862>
2012-05-23 15:24:19 +0000

    Cadastro de Trote

---
+++ Codigo/SiGAT/SiGAT/Models/Negocio/NegocioEndereço.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiGAT.Models.Negocio
+{
+    public class NegocioEndereço : Controller
+    {
+        //
+        // GET: /NegocioEndereço/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
