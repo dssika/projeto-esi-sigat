fd51a1df747ccd8c9cf3477c3c60cf2891ffc400
jessica.si.ufs@gmail.com <jessica.si.ufs@gmail.com@402bc129-2925-e306-34ad-f945a9757862>
2012-10-03 14:01:45 +0000

    acho q faltava essa classe: PessoaModel

---
+++ trunk/Codigo/SiGAT/SiGAT/Models/PessoaModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SiGAT.Models
+{
+    public class PessoaModel
+    {
+        public int cnh { get;set; }
+        public int cpf { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        
+    }
+}
