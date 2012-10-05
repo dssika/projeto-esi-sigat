dbea345796b3d96aa8423395763ee944b44d839c
jessica.si.ufs@gmail.com <jessica.si.ufs@gmail.com@402bc129-2925-e306-34ad-f945a9757862>
2012-10-05 14:19:24 +0000

    classe de teste - NegocioPessoaTest - em construção

--- Codigo/SiGAT/SiGAT/Controllers/HomeController.cs
+++ Codigo/SiGAT/SiGAT/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace SiGAT.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Bem Vindo ao SiGAT!";
 
             return View();
         }
