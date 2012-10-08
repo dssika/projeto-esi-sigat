d9ed1f595ddb8fefa3d68b87ccc0200089eac71b
jessica.si.ufs@gmail.com <jessica.si.ufs@gmail.com@402bc129-2925-e306-34ad-f945a9757862>
2012-10-08 14:57:37 +0000

    models

---
+++ Codigo/SiGAT/SiGAT/Models/CidadeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Resources;
+
+namespace SiGAT.Models
+{
+    public class CidadeModel
+    {
+        [Required]
+        public int idCidade { get; set; }
+        public int idEstado { get; set; }
+        [Required]
+        public string nome { get; set; }
+    }
+}
