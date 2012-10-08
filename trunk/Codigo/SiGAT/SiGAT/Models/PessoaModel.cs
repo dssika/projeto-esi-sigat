100a2a0c021ee9221e49caf89ef79c181e29ee76
jessica.si.ufs@gmail.com <jessica.si.ufs@gmail.com@402bc129-2925-e306-34ad-f945a9757862>
2012-10-08 13:55:06 +0000

    arquivo mensagem

---
+++ trunk/Codigo/SiGAT/SiGAT/Models/PessoaModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Resources;
+
+namespace SiGAT.Models
+{
+    public class PessoaModel
+    {
+        [Required]
+        public int IDPessoa { get; set; }
+        //[Required (ErrorMessage = "Por favor, digite um {0}")]
+        //[DisplayName("nome do autor")]
+        //[StringLength(15, MinimumLength = 5)]
+        //[RegularExpression(@"[A-Za-z]",ErrorMessage="O campo nome não aceita valores numericos")]
+        [Required(ErrorMessageResourceType = typeof(Mensagem), ErrorMessageResourceName = "erro_nome_requerido")]
+        public string Nome { get; set; }
+        [Required]
+        public int cnh { get; set; }
+        [Required]
+        public int cpf { get; set; }
+        [Required]
+        public string email { get; set; }
+        [Required]
+        public DateTime dataNascimento { get; set; }
+        
+        public string logradouro{ get; set; }
+        public string bairro{ get; set; }
+        public string cep{ get; set; }
+
+        [Required]
+        public bool isPM { get; set; }
+
+    }
+}
